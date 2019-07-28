5b01ef7e726e4b5c0844feb215475d950bcafffa
97635 <976353715@qq.com>
2019-07-28 13:15:32 +0800

    mybatis plus

---
+++ cloud-admin-server/src/main/java/com/cloud/hq/business/CloudAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.hq.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAdminServerApplication.class, args);
+    }
+
+}
--- cloud-common/src/main/java/com/cloud/hq/business/config/MybatisPlusConfig.java
+++ cloud-common/src/main/java/com/cloud/hq/business/config/MybatisPlusConfig.java
@@ -1,10 +1,18 @@
 package com.cloud.hq.business.config;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.io.IOException;
 
 /**
  * @author huangqi
@@ -23,6 +31,30 @@ public class MybatisPlusConfig {
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
+    }
+
+    @Bean
+    public MybatisSqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws IOException {
+        MybatisSqlSessionFactoryBean sqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setGlobalConfig(globalConfig());
+        ResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
+        //设置mapper资源路径
+        sqlSessionFactoryBean.setMapperLocations(resourceResolver.getResources("classpath*:/mapper/**/*.xml"));
+        //设置别名
+        sqlSessionFactoryBean.setTypeAliasesPackage("com.cloud.hq.business.model");
+        return sqlSessionFactoryBean;
+    }
+
+    public GlobalConfig globalConfig() {
+        GlobalConfig globalConfig = new GlobalConfig();
+        GlobalConfig.DbConfig dbConfig = new GlobalConfig.DbConfig();
+        //主键类型
+        dbConfig.setIdType(IdType.AUTO);
+        //驼峰下划线转换
+        dbConfig.setTableUnderline(true);
+        globalConfig.setDbConfig(dbConfig);
+        return globalConfig;
     }
 
 }
